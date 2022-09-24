313683d304b90986d77ec36b23a9a00b4f7e8e43
FIRST_NAME LAST_NAME <lacruz0525@gmail.com>
2022-09-24 19:14:55 +0800

    Commit additional categories and design

--- ProjectActivity3.py
+++ ProjectActivity3.py
@@ -1,24 +1,27 @@
-import urllib.request
+import urllib
 import requests
 
 #chosen REST api
 api = "https://ipapi.co/"
 
-#discover external ip address of router 
+# discover external ip address of router
 ext_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
 
-#url of the chosen api & external ip
+# url of the chosen api & external ip 
 url = api + ext_ip + "/json"
 
-#fetch json data from api and external ip
+# fetch json data from api and external ip
 json_data = requests.get(url).json()
 
-#title for each category
-category = ["IP Address", "Version", "City", "Region", "Country"]
+print ("GROUP 7 - PROJECT # 3\n")
 
-#fetch json data from the api
-json_categories = ["ip", "version", "city", "region", "country_name"]
+# Title for each category
+category = ["IP Address", "Version", "City", "Region", "Country", "Continent Code", "Currency", "Autonomous System Number", "ISP"]
+
+# fetch json data from the api
+json_categories = ["ip", "version", "city", "region", "country_name", "continent_code", "currency", "asn", "org"]
 
 #display output
 for i in range(len(category)):
-    print(category[i] + ": " + json_data[json_categories[i]])
+    print(category[i] +  ": " + json_data[json_categories[i]])
+    print ("===================================")
